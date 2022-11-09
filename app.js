723ab30ef528b4524df666eec601a80ce19aa9e2
Leonel Sosa <leo.vm.cba@gmail.com>
2022-11-09 10:58:24 -0300

    fix navigation

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const btnMenu = document.querySelector('#btn-menu');
 const navBar = document.querySelector('#navbar-default');
+const enlaces = document.querySelectorAll('#navbar-default a');
 
 btnMenu.addEventListener('click', () => {
     navBar.classList.toggle('hidden');
+});
+
+console.log(enlaces);
+
+enlaces.forEach( enlace => {
+    enlace.addEventListener('click', () => {
+        enlaces.forEach(enlace => {
+            enlace.classList.remove('md:text-blue-700');
+        });
+        enlace.classList.add('md:text-blue-700');
+    })
 })
